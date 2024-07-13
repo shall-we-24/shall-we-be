7d279f661fe7a4b99ff902ff46b99807dffff4fa
Kim Dayeong <gloria030114@gmail.com>
2024-07-13 15:24:47 +0900

    initial commit

---
+++ src/main/java/com/shallwe/ShallWeApplication.java
@@ -0,0 +1,13 @@
+package com.shallwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShallWeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShallWeApplication.class, args);
+    }
+
+}
